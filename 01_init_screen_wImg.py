f49c44c328e923912f07a9bb654dd9dc3a0fb2fa
ineszz <ineszz@users.noreply.github.com>
2022-01-15 20:30:06 +0200

    add background with *.png format

--- 01_init_screen_wImg.py
+++ 01_init_screen_wImg.py
@@ -7,13 +7,17 @@ screen = pygame.display.set_mode([800,600])
 keep_going = True
 # add image
 pic = pygame.image.load("CrazySmile.bmp")
+# add background
+ground = pygame.image.load("sky.png")
 
 # game event
 while keep_going:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              keep_going = False
+    screen.blit(ground, (0,0))
     screen.blit(pic, (100,100))
     pygame.display.update()
 
 pygame.quit() # Exit game
+
